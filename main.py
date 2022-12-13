5147439914264f353602abb9d4a892153606e859
anoduck <11767-anoduck@users.noreply.gitgud.io>
2022-12-13 18:20:05 -0500

    removed unused packages

--- main.py
+++ main.py
@@ -18,8 +18,8 @@
 # ╚══════╝╚═╝  ╚═══╝╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝
 # ========================================================================================================================================
 
-import numpy as np
-import scipy.interpolate as si
+# import numpy as np
+# import scipy.interpolate as si
 
 from selenium import webdriver
 # from selenium.webdriver import Firefox
@@ -225,43 +225,43 @@ def wait_between(a, b):
 #/_/  /_/\___/\_,_/___/\__/ /_/  /_/\___/___/\__/_/_/_/\__/_//_/\__/___/
 # -----------------------------------------------------------------------
 # Using B-spline for simulate humane like mouse movments
-def human_like_mouse_move(action, start_element):
-    points = [[6, 2], [3, 2], [0, 0], [0, 2]];
-    points = np.array(points)
-    x = points[:, 0]
-    y = points[:, 1]
+# def human_like_mouse_move(action, start_element):
+#     points = [[6, 2], [3, 2], [0, 0], [0, 2]];
+#     points = np.array(points)
+#     x = points[:, 0]
+#     y = points[:, 1]
 
-    t = range(len(points))
-    ipl_t = np.linspace(0.0, len(points) - 1, 100)
+#     t = range(len(points))
+#     ipl_t = np.linspace(0.0, len(points) - 1, 100)
 
-    x_tup = si.splrep(t, x, k=1)
-    y_tup = si.splrep(t, y, k=1)
+#     x_tup = si.splrep(t, x, k=1)
+#     y_tup = si.splrep(t, y, k=1)
 
-    x_list = list(x_tup)
-    xl = x.tolist()
-    x_list[1] = xl + [0.0, 0.0, 0.0, 0.0]
+#     x_list = list(x_tup)
+#     xl = x.tolist()
+#     x_list[1] = xl + [0.0, 0.0, 0.0, 0.0]
 
-    y_list = list(y_tup)
-    yl = y.tolist()
-    y_list[1] = yl + [0.0, 0.0, 0.0, 0.0]
+#     y_list = list(y_tup)
+#     yl = y.tolist()
+#     y_list[1] = yl + [0.0, 0.0, 0.0, 0.0]
 
-    x_i = si.splev(ipl_t, x_list)
-    y_i = si.splev(ipl_t, y_list)
+#     x_i = si.splev(ipl_t, x_list)
+#     y_i = si.splev(ipl_t, y_list)
 
-    startElement = start_element
+#     startElement = start_element
 
-    action.move_to_element(startElement);
-    action.perform();
+#     action.move_to_element(startElement);
+#     action.perform();
 
-    c = 5  # change it for more move
-    i = 0
-    for mouse_x, mouse_y in zip(x_i, y_i):
-        action.move_by_offset(mouse_x, mouse_y);
-        action.perform();
-        # self.log("Move mouse to, %s ,%s" % (mouse_x, mouse_y))
-        i += 1
-        if i == c:
-            break;
+#     c = 5  # change it for more move
+#     i = 0
+#     for mouse_x, mouse_y in zip(x_i, y_i):
+#         action.move_by_offset(mouse_x, mouse_y);
+#         action.perform();
+#         # self.log("Move mouse to, %s ,%s" % (mouse_x, mouse_y))
+#         i += 1
+#         if i == c:
+#             break;
 
 
 #  _____          __      __
